05cc11d0bc144582f0777cd1fdc839bf14d74c54
Murilo Henrique <77072462+murilo-henrique060@users.noreply.github.com>
2023-06-07 14:15:39 -0300

    DRIVER AASDASD KMDSAVCKMSDFKSFPOKJNDF

--- src/util/Conexao.java
+++ src/util/Conexao.java
@@ -15,7 +15,7 @@ public class Conexao {
 
     public Conexao(String servidor, String database, String user, String password) throws SQLException {
         try {
-            Class.forName("org.gjt.mm.mysql.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://" + servidor + "/" + database, user, password);
         } catch (ClassNotFoundException ex) {
             System.out.println("Nao foi possıvel encontrar a classe do " + "Driver do MySQL");
