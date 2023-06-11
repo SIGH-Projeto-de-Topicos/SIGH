d4359522f27969cc263feb988972e909ae48943e
Henrique Belém <Henrique Belm@Henrique>
2023-06-11 11:05:57 -0300

    pq esse JS n atualiza?

--- webcontent/js/index.js
+++ webcontent/js/index.js
@@ -2,22 +2,25 @@ function loginForm() {
 	let loginContainer = document.getElementById("login-container");
 	
 	loginContainer.innerHTML =
-		"<div id=\"login-overlay\"></div>" + 
-		"<div id=\"login\">" +
-			"<h1>Entrar</h1>" +
-			"<form method=\"POST\" action=\"loginResponse.jsp\">" +
-				"<div class=\"form-group\">" +
-					"<label class=\"form-label\" for=\"email\">E-mail</label>" +
-					"<input placeholder=\"Entre com seu e-mail...\" name=\"emailf\" type=\"email\" id=\"email\" class=\"form-control\" value=\"\">" +
-				"</div>" +
-				"<div class=\"form-group\">" +
-					"<label class=\"form-label\" for=\"password\">Senha</label>" +
-					"<input placeholder=\"Entre com sua senha...\" name=\"passwordf\" type=\"password\" id=\"password\" class=\"form-control\" value=\"\">" +
-				"</div>" +
-				"<a href=\"#\">Esqueci minha senha</a>" +
-				"<button class=\"button2\">Entrar</button>" +
-			"</form>" +
-		"</div>";
+	"<div id='login-overlay'></div>" + 
+  		"<div id='login'>" +
+    	"<h1>Entrar</h1>" +
+   		"<form method='POST' action='loginResponse.jsp'>" +
+      			"<div class='form-group'>" +
+        		"<label class='form-label' for='email'>E-mail</label>" +
+       			"<input placeholder='Entre com seu e-mail...' name='email' type='email' id='email' class='form-control' value=''>" +
+      	"</div>" +
+//
+      "<div class='form-group'>" +
+        	"<label class='form-label' for='password'>Senha</label>" +
+        	"<input placeholder='Entre com sua senha...' name='password' type='password' id='password' class='form-control' value=''>" +
+      "</div>" +
+//
+      	"<a href='#'>Esqueci minha senha</a>" +
+      	"<button class='button2'>Entrar</button>" +
+    	"</form>" +
+//
+  "</div>";
 	
 	let loginOverlay = document.getElementById("login-overlay");
 	loginOverlay.addEventListener("click", close);
